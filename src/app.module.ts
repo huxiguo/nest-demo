09a330bc03f94d1787b9aa7e2e2f43a8de3e975a
huxiguo <hu@seekhoo.cn>
2023-11-21 22:38:49 +0800

    cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DemoController } from './demo/demo.controller';
+import { DemoModule } from './demo/demo.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [DemoModule],
+  controllers: [AppController, DemoController],
   providers: [AppService],
 })
 export class AppModule {}
