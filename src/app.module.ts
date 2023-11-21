fbecda87937e5377c845a33218b2de4f9d08ad9f
huxiguo <hu@seekhoo.cn>
2023-11-21 23:26:46 +0800

    增删改查生成器

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DemoController } from './demo/demo.controller';
 import { DemoModule } from './demo/demo.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [DemoModule],
+  imports: [DemoModule, UserModule],
   controllers: [AppController, DemoController],
   providers: [AppService],
 })
